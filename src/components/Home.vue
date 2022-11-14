656f74370f906702ceb58d76fd2feb84b2d02999
yuumm <15802883812@163.com>
2022-11-14 22:40:56 +0800

    ‘init’

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
-        Home 组件
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    methods: {
+        logout() {
+            // 清空localstorage
+            localStorage.removeItem("userInfo");
+            this.$router.push("/login");
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
